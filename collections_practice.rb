922e259c3d1c32e38baa1a5906281dbae7549936
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-19 21:03:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
 # your code goes here
+
+def begins_with_r(array)
+ # array.each do |item|
+ # if item.start_with?("r") == true
+  #  
+  #end
+end
+
+def contain_a(array)
+  array.map do |item|
+  item.include?("a") 
+  end
+end
+
+def first_wa(array)
+array.each do |item|
+  item.start_with?("wa") 
+  end
+end
+
+def remove_non_strings
+  
+end
+
+def count_elements
+end
+
+def merge_data
+end
+
+def find_cool
+end
+
+def organize_schools
+end
